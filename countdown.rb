a5e4a4d1ba8c5e82061bed5f094fc38eddc899b8
Zoe Keller <keller.zoe98@gmail.com>
2020-05-27 20:28:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,19 +1,15 @@
 #write your code here
 
 def countdown(number)
-  number == 12
   while number > 0
   sleep 1
     puts "#{number} SECOND(S)!"
+    number -= 1
   end
-  number -= 1 
-  while number == 0 
-    print "HAPPY NEW YEAR!"
-  end
+  return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(5)
-  number == 12
+def countdown_with_sleep(number)
   while number > 0
     print "#{number} SECOND(S)!"
   end
